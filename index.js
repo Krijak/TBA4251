bc61b7e867645d34f928a2634d4d7cd64e137bd8
krijak <krijak@stud.ntnu.no>
2016-11-15 21:08:43 +0100

    mer db-stuff

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
+var fs = require('fs');
+var mongoose = require('mongoose');
 var app = express();
-var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+var path = require('path');
+
+// var MongoClient = require('mongodb').MongoClient, assert = require('assert');
 
 
 app.set('port', (process.env.PORT || 5000));
@@ -27,8 +31,24 @@ app.listen(app.get('port'), function() {
 // Connection URL 
 var url = 'mongodb://heroku_k7jjmzwc:idaj27pbptblpb7f1nh3oiiqu9@ds139327.mlab.com:39327/heroku_k7jjmzwc';
 // Use connect method to connect to the Server 
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-  db.close();
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected correctly to server");
+//   db.close();
+// });
+
+mongoose.connect(url);
+mongoose.connection.on('connected', function () {  
+  console.log('connected correctly to server');
+});
+
+//load all files in models dir
+fs.readdirSync(__dirname + '/models').forEach(function(filename) {
+  if (~filename.indexOf('.js')) require(__dirname + '/models/' + filename)
+});
+
+app.get('/users', function(req, res) {
+  mongoose.model('users').find(function(err, users) {
+    res.send(users);
+  });
 });
